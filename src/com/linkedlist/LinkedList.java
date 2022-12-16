f8872d1c1d44d907c3b15386398ea053215e9bac
jayashri23 <jsjankar23@gmail.com>
2022-12-17 04:15:34 +0530

    Deleting First element from linked list

--- src/com/linkedlist/LinkedList.java
+++ src/com/linkedlist/LinkedList.java
@@ -1,61 +1,84 @@
 package com.linkedlist;
 
 public class LinkedList {
-    class Node{
+    class Node {   // Create a node
         int data;
         Node next;
-        Node(int d){
+
+        Node(int d) {
             data = d;
             next = null;
         }
     }
-    public Node head = null;
-    public Node tail = null;
-
+    public Node head=null;
+    public Node tail=null;
     public void addNode(int d){
-        Node newNode = new Node(d);
+        Node newNode=new Node(d);
 
-        if (head == null){
+
+        if (head == null) {
             head = newNode;
-        }
-        else {
+        } else {
+
             tail.next = newNode;
-            newNode = null;
+            newNode.next=null;
+
         }
-        tail =newNode;
+        tail = newNode;
+
     }
-    public void insertBetweenNode(int d){
-        System.out.println("Updated Linked list: ");
-        Node newNode = new Node(d);
+    public void insertBetweenNode(int a){
+        Node newNode=new Node( a);
+        Node current=head;
+        while(current!=null) {
+            if (current.data != 56) {
+                current = current.next;
+            } else if (current.data == 56)
+                break;
+        }
+        Node temp=current.next;
+        newNode.next=temp;
+        current.next=newNode;
+    }
+
+    public void deleteElement(){
+        if(this.head!=null) {
+            Node temp=this.head;
+            this.head=this.head.next;
+            temp= null;
+        }
+
+
+    }
+
+
+
+    public void display() {                 // Print the linked list
         Node current = head;
-        while (current != null){
-            if (current.data != 56){
-                current =current.next;
-            }else if(current.data == 56)
-                    break;
-            }
-        Node temp =current.next;
-        newNode.next =temp;
-        current.next = newNode;
+
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
         }
-        public void display(){
-        Node current =head;
-        if (head  == null) {
-            System.out.println("list is empty");
-        }
-            System.out.println("Linked list node:");
-        while (current != null){
-            System.out.println(current.data+ "");
+        System.out.println("Nodes of linked list ");
+        while (current != null) {
+            System.out.print(current.data + " ");
             current = current.next;
         }
-    }
 
+    }
     public static void main(String[] args) {
-        LinkedList linkedList = new LinkedList();
-        linkedList.addNode(56);
-        linkedList.addNode(70);
-        linkedList.display();
-        linkedList.insertBetweenNode(30);
-        linkedList.display();
+        LinkedList li = new LinkedList();
+        System.out.println("Linked list: ");
+        li.addNode(56);
+        li.addNode(70);
+        li.display();
+        System.out.println("\nAdded new element:");
+        li.insertBetweenNode(30);
+        li.display();
+        System.out.println("\n\nAfter deleting first element:");
+        li.deleteElement();
+        li.display();
+
     }
 }
